a5b55747edea3653e1dea53ea22167c2bbb405b5
Marvin W <marvwood12@gmail.com>
2022-06-30 22:34:41 -0400

    implement saving user records

--- repositories/users.js
+++ repositories/users.js
@@ -16,14 +16,25 @@ class UsersRepository {
 
   async getAll() {
     //Open the file called this.filename
-    return contents = JSON.parse(await fs.promises.readFile(this.filename, {
-      encoding: 'utf8',
-    }));
+    return JSON.parse(
+      await fs.promises.readFile(this.filename, {
+        encoding: 'utf8',
+      })
+    );
+  }
+
+  async create(attrs) {
+    const records = await this.getAll();
+    records.push(attrs);
+    // write the updared 'records' array back to this.filename
+    await fs.promises.writeFile(this.filename, JSON.stringify(records));
   }
 }
 
 const test = async () => {
   const repo = new UsersRepository('users.json');
+
+  await repo.create({ email: 'test@test.com', password: 'password' });
 
   const users = await repo.getAll();
 
