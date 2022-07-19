3b6e4553c6fbf0a0481c386ff6c368ae7d0c633c
Marvin W <marvwood12@gmail.com>
2022-07-19 19:54:45 -0400

    filtering records

--- repositories/users.js
+++ repositories/users.js
@@ -66,12 +66,30 @@ class UsersRepository {
     Object.assign(record, attrs);
     await this.writeAll(records);
   }
+
+  async getOneBy(filters) {
+    const records = await this.getAll();
+    for (let record of records) {
+      let found = true;
+      for (let key in filters) {
+        if (record[key] !== filters[key]) {
+          found = false;
+        }
+      }
+
+      if (found) {
+        return record;
+      }
+    }
+  }
 }
 
 const test = async () => {
   const repo = new UsersRepository('users.json');
 
-  await repo.update('64f83', { password: 'mypassword' });
+  const user = await repo.getOneBy({ email: 'test@test.com', password: 'myddsfsdfspassword' });
+
+  console.log(user);
 };
 
 test();
