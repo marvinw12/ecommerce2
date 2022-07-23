4632ab737a26d5f02df6eae1a9c89aacc316649c
Marvin W <marvwood12@gmail.com>
2022-07-23 12:34:59 -0400

    signout a user

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cookieSession = require('cookie-session');
 const usersRepo = require('./repositories/users');
+const { reset } = require('nodemon');
 
 const app = express();
 
@@ -12,7 +13,7 @@ app.use(
   })
 );
 
-app.get('/', (req, res) => {
+app.get('/signup', (req, res) => {
   res.send(`
     <div>
         Your id is ${req.session.userId}
@@ -26,7 +27,7 @@ app.get('/', (req, res) => {
   `);
 });
 
-app.post('/', async (req, res) => {
+app.post('/signup', async (req, res) => {
   const { email, password, passwordConfirmation } = req.body;
 
   const existingUser = await usersRepo.getOneBy({ email });
@@ -44,6 +45,25 @@ app.post('/', async (req, res) => {
 
   res.send('Account Created');
 });
+
+app.get('/signout', (req, res) => {
+  req.session = null;
+  res.send('You are logged out');
+});
+
+app.get('/signin', (req, res) => {
+  res.send(`
+  <div>
+  <form method="POST">
+      <input name="email" placeholder="email" />
+      <input name="password" placeholder="password" />
+      <button>Sign In</button>
+  </form>
+</div>
+  `);
+});
+
+app.post('/signin', async)
 
 app.listen(3000, () => {
   console.log('Listening');
