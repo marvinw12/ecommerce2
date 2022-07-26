0f3d73b8969761e4713deaf90fa977a672571479
Marvin W <marvwood12@gmail.com>
2022-07-26 00:23:30 -0400

    styling added

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const cookieSession = require("cookie-session");
-const authRouter = require("./routes/admin/auth");
+const express = require('express');
+const bodyParser = require('body-parser');
+const cookieSession = require('cookie-session');
+const authRouter = require('./routes/admin/auth');
 
 const app = express();
 
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cookieSession({
-    keys: ["eirufnefiun23edn"],
+    keys: ['eirufnefiun23edn'],
   })
 );
 app.use(authRouter);
 
 app.listen(3000, () => {
-  console.log("Listening");
+  console.log('Listening');
 });
