a5ab33132cae9d41e6e770aa47fe70fe558a39ab
Marvin W <marvwood12@gmail.com>
2022-08-06 14:07:35 -0400

    "add to cart" submission

--- routes/carts.js
+++ routes/carts.js
@@ -3,6 +3,9 @@ const express = require('express');
 const router = express.Router();
 
 //receive a POST request to add an item  to a card
+router.post('/cart/products', async (req, res) => {
+    conso
+})
 
 // Receive a GET request to show all items in cart
 
