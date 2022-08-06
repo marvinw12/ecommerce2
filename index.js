6e4a818a8e2fda2af89c0018e32a78ef6bad8be4
Marvin W <marvwood12@gmail.com>
2022-08-06 11:16:19 -0400

    products router + products index

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cookieSession = require('cookie-session');
 const authRouter = require('./routes/admin/auth');
-const productRouter = require('./routes/admin/products');
+const adminProductsRouter = require('./routes/admin/products');
+const productsRouter = require('./routes/products');
 
 const app = express();
 
@@ -14,7 +15,8 @@ app.use(
   })
 );
 app.use(authRouter);
-app.use(productRouter);
+app.use(adminProductsRouter);
+app.use(productsRouter);
 
 app.listen(3000, () => {
   console.log('Listening');
---
+++ views/products/index.js
@@ -0,0 +1,17 @@
+module.exports = ({ products }) => {
+  const renderedProducts = products
+    .map((product) => {
+      return `
+        <li>${product.title} - ${product.price}</li>
+        `;
+    })
+    .join('');
+
+
+
+return `
+    <ul>
+    ${renderedProducts}
+    </ul>
+`;
+};
